21e9aeef34511b81e7944604e2ec6fc6e8bf7279
Daniel Rodriguez <99danielrod@gmail.com>
2020-10-15 19:39:17 -0500

    funcionalidad completa

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, url_for, render_template, request
 import numpy as np
 import matplotlib.pyplot as plt
 from pulp import *
-
+import json
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,7 +16,7 @@ def informacion():
 def calcular():
     if request.method== 'POST':
         fullData=request.get_json()
-        print(fullData)
+        #print(fullData)
         maxomin=fullData['maxomin']
         formaCanonica=fullData['canonica']
         todasRestricciones=fullData['restricciones']
@@ -61,7 +61,6 @@ def calcular():
         resultadoTexto+=f"Resultado funcion Objetivo: {value(PROBLEMA.objective)}"
         resFobj=value(PROBLEMA.objective)
         resultadoFunObj={"x1":x1Fobj,"x2":x2Fobj,"resultado":resFobj}
-        print(resultadoFunObj) ##################################################
         """
         Puntos
         """
@@ -128,11 +127,90 @@ def calcular():
                         exito=False
                         break  
             if exito:
-                newPuntos.append(todosPuntos[another])            
-        print(newPuntos) 
+                newPuntos.append(todosPuntos[another])
         """
         Graficar
         """
-        return 'data collected' #aqui se deben de devolver los resultados
+        maxX=20.0
+        maxY=20.0
+        for otherX in range(len(todasRestricciones)):
+            theX1=float(todasRestricciones[otherX]["x1"])
+            theX2=float(todasRestricciones[otherX]["x2"])
+            resul=float(todasRestricciones[otherX]["resultado"])
+            if theX2!=0.0:
+                maximoEnY=(resul/theX2)+((theX1/theX2)*-1)
+                if maximoEnY>maxY:
+                    maxY=int(maximoEnY)+10
+            if theX1!=0.0:
+                maximoEnX=(resul/theX1)+((theX2/theX1)*-1)
+                if maximoEnX>maxX:
+                    maxX=int(maximoEnX)+10
+
+        x = np.linspace(0, maxX, 2000)
+        allYs=[]
+        posi=0
+        for otherX in range(len(todasRestricciones)):
+            resul=float(todasRestricciones[otherX]["resultado"])
+            rX1=float(todasRestricciones[otherX]["x1"])*-1
+            rX2=float(todasRestricciones[otherX]["x2"])
+            if rX2!=0.0:
+                operacion=(resul/rX2)+((rX1/rX2)*x)
+                allYs.append(operacion)
+                simbolo=todasRestricciones[otherX]["igualador"]
+                plt.plot(x, operacion, label=f"{str(rX1*-1)}X1+{str(rX2)}X2{simbolo}{str(resul)}")
+                if simbolo=="<=":
+                    plt.fill_between(x, operacion, where=operacion<=operacion, alpha=0.5)
+                elif simbolo==">=":
+                    plt.fill_between(x, operacion, where=operacion>=operacion, alpha=0.5)
+            else:
+                posi=0
+                operacion=np.arange(2000)*0
+                resultado=resul/(rX1*-1)
+                for b in range(len(x)):
+                    if resultado==x[b]:
+                        operacion[b]=maxY
+                        posi=b
+                        break
+                    if x[b]>resultado and b!=0:
+                        operacion[b-1]=maxY
+                        posi=b-1
+                        break
+
+                for c in range(len(operacion)):
+                    if simbolo == "<=":
+                        if c>=posi:
+                            operacion[c]=maxY
+                    elif simbolo == ">=":
+                        if c<=posi:
+                            operacion[c]=maxY
+                allYs.append(operacion)
+                plt.plot(x, operacion, label=f"{str(rX1*-1)}X1+{str(rX2)}X2={str(resul)}")
+                if simbolo=="<=":
+                    plt.fill_between(x, operacion, where=operacion<=operacion, alpha=0.5)
+                elif simbolo==">=":
+                    plt.fill_between(x, operacion, where=operacion>=operacion, alpha=0.5)
+        plt.xlim((0, maxX))
+        plt.ylim((0, maxY))
+        pX1=float(formaCanonica['x1'])
+        pX2=float(formaCanonica['x2'])
+        if pX2!=0.0:
+            funcionObjetivo=(resFobj/pX2)+(((pX1*-1)/pX2)*x)
+        else:
+            funcionObjetivo=np.arange(2000)*0
+        plt.plot(x,funcionObjetivo,color="black", label=f"Fun. Obj:{str(pX1)}X1+{str(pX2)}X2={str(resFobj)}")
+        plt.xlabel('X1')
+        plt.ylabel('X2')
+        puntos="Puntos:\n"
+        for a in range(len(newPuntos)):
+            plt.scatter(newPuntos[a][0],newPuntos[a][1], color="#333333")
+            puntos+=f"[ {newPuntos[a][0]} , {newPuntos[a][1]} ]\n"
+        plt.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0.)
+        plt.savefig('grafica.png')
+        """
+        Fin Grafica
+        """
+        resultados={"resFunObj":resultadoFunObj,"puntos": newPuntos}
+        print(resultados)
+        return json.dumps(resultados)
 if __name__=='__main__':
     app.run(debug=True)
